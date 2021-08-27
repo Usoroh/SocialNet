a8c2875a16915f77112793f94782c40debf16823
Usoroh <paius.usoroh@gmail.com>
2021-08-27 20:12:11 +0600

    новые эндпоинты

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Здарова, азиат!</p>"
+def home():
+    return "<p>Здарова, азиат!</p><div><a href='/login'>Войди на сайт</a></div>"
+
+@app.route("/login")
+def login():
+    return "<p>Здесь будет авторизация</p><div><a href='/register'>Новенький? Зарегайся</a></div>"
+
+@app.route("/register")
+def register():
+    return "<p>Здесь будет регистрация</p><div><a href='/login'>Уже зареган? Войди на сайт</a>"
+
+@app.route("/<name>")
+def hello(name):
+    return f"Cтраницы {escape(name)} не существует!"
+   
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
